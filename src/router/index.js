3ceea4ad539e88ad16e4190a2a35ffa1ebb2a8b0
Fernanda019 <fernanda05@gmail.com>
2026-05-29 00:27:35 -0500

    Configuracion de vistas y rutas principales

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+import Home from "../views/Home.vue";
+import Create from "../views/Create.vue";
+import Edit from "../views/Edit.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/edit/:id",
+    name: "Edit",
+    component: Edit,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    <h1>Create</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "CreateView",
 };
 </script>
