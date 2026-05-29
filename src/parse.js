0ff39507d188b690dfdbf45b2fdd07e784199ae7
Fernanda019 <fernanda05@gmail.com>
2026-05-29 00:44:12 -0500

    Configuracion de Parse y Back4App

---
+++ src/parse.js
@@ -0,0 +1,10 @@
+import Parse from "parse/dist/parse.min.js";
+
+Parse.initialize(
+  "hRdSvbzvvxM6kdOsUH5PgtpjRAXuyEtsZ4hAcIoK",
+  "XdjbaQzT9bJuKJsaUx49HokEjWRocWwg7ewQcpPW"
+);
+
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+export default Parse;
